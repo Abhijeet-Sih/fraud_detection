c497dc53e1c63386e79e2626d76ebb87341f7efe
Abhijeet-Sih <77975708+Abhijeet-Sih@users.noreply.github.com>
2023-06-18 00:00:59 +0530

    Added some more functions

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,9 @@
 
 print("adding all the data processing code related to fraud detection")
+
+a = 5
+b = 7
+def sum_num(x,y):
+    c=x+y
+    return c
+print("sum of the given two numbers is:", sum_num(a,b))
